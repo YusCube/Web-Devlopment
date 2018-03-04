40a686e7592e7b945b566b5db03aedf32c0d8197
Shiva_Saran <shiva3104@gmail.com>
2018-03-04 23:49:53 +0530

    Upload

---
+++ Udemy/JavaScript/stalker.js
@@ -0,0 +1,5 @@
+var userName = prompt("Enter your first Name");
+var lastName = prompt("Enter your last Name");
+var age = prompt("Enter your age");
+console.log("Your full name is "+userName+" "+lastName);
+console.log("You are "+age+" years old.");
