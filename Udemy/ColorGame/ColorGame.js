0c4c204c643b52c28061bbf8827fee23e714732c
Shiva_Saran <shiva3104@gmail.com>
2018-03-14 13:22:20 +0530

    Alpha Version of Game

--- Udemy/ColorGame/ColorGame.js
+++ Udemy/ColorGame/ColorGame.js
@@ -4,32 +4,61 @@ var colors = createColors(6);
 var rgbText = document.querySelector("#rgb");
 var pickedColor = colors[randomNumber(6)];
 var h1Header = document.querySelector("h1");
+var h2Header = document.querySelector("h2");
+// var h2SubHeader = document.querySelector("#find");
 var message = document.querySelector("#message");
+var playButton = document.querySelector("#btn");
+var easyButton = document.querySelector("#easy");
+var hardButton = document.querySelector("#hard");
+var level;
+
+easyButton.addEventListener("click", function(){
+	this.classList.add("selected");
+	hardButton.classList.remove("selected");
+	level = 3;
+	for(var i = 3;i < squares.length; i++)
+		squares[i].style.display = "none";
+	reset();
+});
+
+hardButton.addEventListener("click", function(){
+	this.classList.add("selected");
+	easyButton.classList.remove("selected");
+	level = 6;
+	for(var i = 3;i < squares.length; i++)
+		squares[i].style.display = "block";
+	reset();
+});
 
 //
-for(var i = 0;i < squares.length; i++){
-	//Set Square Colors
-	squares[i].style.background = colors[i];
-	
-	//Listen for clicks from the squares
-	squares[i].addEventListener("click", function(){
-		//rgbText.textContent = this.style.background;
-		var clickedColor = this.style.background;
-		if (clickedColor === pickedColor){
-			rgbText.textContent = this.style.background;
-			h1Header.style.backgroundColor = clickedColor;
-			message.textContent = "Correct!!!";
-			for(var i = 0;i < squares.length; i++){
-				squares[i].style.background = clickedColor;
+function playGame(){
+	for(var i = 0;i < squares.length; i++){
+		//Set Square Colors
+		squares[i].style.background = colors[i];
+		h1Header.style.backgroundColor = "rgb("+randomNumber(256)+", "+randomNumber(256)+", "+randomNumber(256)+")";
+		
+		//Listen for clicks from the squares
+		squares[i].addEventListener("click", function(){
+			var clickedColor = this.style.background;
+			if (clickedColor === pickedColor){
+				rgbText.textContent = this.style.background;
+				h1Header.style.backgroundColor = clickedColor;
+				h2Header.textContent = "You have found this "+clickedColor+ " color!!!";
+				message.textContent = "Correct!!!";
+				for(var i = 0;i < squares.length; i++){
+					squares[i].style.background = clickedColor;
+				}
 			}
-		}
-		else
-			this.style.background = document.body.style.backgroundColor;
-			message.textContent = "Try Again :(";
-
-		return (clickedColor, pickedColor);
-	});
+			else{
+				this.style.background = document.body.style.backgroundColor;
+				message.textContent = "Try Again :(";
+			}
+			return (clickedColor, pickedColor);
+		});
+	}
 }
+
+playButton.addEventListener("click", reset);
 
 //Generate Random Number upto range 'n'
 function randomNumber(n){
@@ -43,4 +72,12 @@ function createColors(l){
 		tempCol.push("rgb("+randomNumber(256)+", "+randomNumber(256)+", "+randomNumber(256)+")");
 	}
 	return tempCol;
+}
+
+function reset(){
+	colors = createColors(level);
+	pickedColor = colors[randomNumber(level)];
+	h2Header.textContent = "Find "+pickedColor+" color";
+	message.textContent = "";
+	playGame();
 }
