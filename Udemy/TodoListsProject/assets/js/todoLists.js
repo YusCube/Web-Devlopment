b9221aaa15c1fc94b48f3c6ab12709906e4385d2
Shiva_Saran <shiva3104@gmail.com>
2018-03-17 13:27:33 +0530

    Basic todo Structure done

--- Udemy/TodoListsProject/assets/js/todoLists.js
+++ Udemy/TodoListsProject/assets/js/todoLists.js
@@ -1,1 +1,19 @@
-alert("connected");
+//Check off specific todo list
+$("ul").on("click", "li", function(){		//click listener will work on the ul only when li is clicked
+	$(this).toggleClass("completed");
+});
+
+//Delete off specific todo
+$("ul").on("click", "span", function(event){
+	$(this).parent().fadeOut(500, function(){$(this).remove();});
+	event.stopPropagation();
+});
+
+$("input[type='text']").keypress(function(event){
+	if (event.which === 13 && $(this).val() !== ""){
+		var todo = $(this).val();
+		$(this).val("");
+		$("ul").append("<li><span>X</span> " +todo+ "</li>");
+
+	}
+});
